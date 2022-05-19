82fcba9e8b567f88bd948e9bd37a3d112a6e76fc
ModyQyW <wurui-dev@foxmail.com>
2022-05-19 18:57:15 +0800

    chore: update examples config

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslint from '@modyqyw/vite-plugin-eslint';
 import stylelint from 'vite-plugin-stylelint';
+import inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint(), stylelint()],
+  plugins: [vue(), eslint(), stylelint(), inspect()],
 });
