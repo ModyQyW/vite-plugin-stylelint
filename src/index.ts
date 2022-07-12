8a6b03c3bf47ee4ac18aa63459a82eeaede2b755
ModyQyW <wurui-dev@foxmail.com>
2022-07-12 16:02:43 +0800

    fix: fix type

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 
 export type FilterPattern = string | string[];
 
-export interface VitePluginStylelintOptions extends Stylelint.LinterOptions {
+export interface StylelintPluginOptions extends Stylelint.LinterOptions {
   cache?: boolean;
   cacheLocation?: string;
   include?: FilterPattern;
@@ -19,7 +19,7 @@ export interface VitePluginStylelintOptions extends Stylelint.LinterOptions {
   emitWarningAsError?: boolean;
 }
 
-export default function StylelintPlugin(options: VitePluginStylelintOptions = {}): Vite.Plugin {
+export default function StylelintPlugin(options: StylelintPluginOptions = {}): Vite.Plugin {
   const cache = options?.cache ?? true;
   const cacheLocation =
     options?.cacheLocation ?? path.join('node_modules', '.vite', 'vite-plugin-stylelint');
