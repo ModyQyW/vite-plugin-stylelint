608b72434eebf88787bd2e2a6038c3752086bf14
ModyQyW <wurui-dev@foxmail.com>
2023-01-30 21:40:08 +0800

    chore: remove example eslint config

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from '@modyqyw/vite-plugin-eslint';
 import stylelint from 'vite-plugin-stylelint';
 import inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), stylelint(), inspect()],
+  plugins: [react(), stylelint(), inspect()],
 });
