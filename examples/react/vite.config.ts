fe31a778fe5fcac03909f5473dea96f09dcf1630
Rui Wu <wurui.dev@gmail.com>
2024-10-12 18:11:16 +0800

    chore: remove examples, add back later

--- examples/react/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import stylelint from 'vite-plugin-stylelint';
-import inspect from 'vite-plugin-inspect';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), stylelint(), inspect()],
-});
--- examples/vue/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import stylelint from 'vite-plugin-stylelint';
-import inspect from 'vite-plugin-inspect';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), stylelint(), inspect()],
-});
